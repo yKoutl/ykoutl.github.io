fd72b1b30bae9e10a33bf4022e232bc706ca60be
yKoutl <raulquintanazinc@gmail.com>
2026-01-09 15:28:51 -0500

    Proyecto portafolio inicial con Vite + Tailwind v4

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="h-screen bg-black flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-cyan-400">
+        Tailwind 4 funcionando 😎
+      </h1>
+    </div>
+  );
+}
